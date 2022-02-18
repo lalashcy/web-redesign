c0f3bd5985030074e1b45736635844d39f5a92cf
lalashcy <000scy12@gmail.com>
2022-02-18 17:08:18 +0900

    프리즈마

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $(".slider").slick({
+      autoplay: true ,
+      autoplaySpeed: 1000 ,
+      arrows: false ,
+      dots: true ,
+      pauseOnHover: true ,
+      slidesToShow: 1 ,
+      slidesToScroll: 1, 
+      speed: 500 ,
+    });
+    
+  });
+  
